bf7f5ff4f8e5d86f491f513966129dc4818dea14
Fatma Nur Iskal <fnuriskal@gmail.com>
2026-06-17 14:18:17 +0300

    Add project files.

---
+++ FinancialThrottle.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinancialThrottle.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FinancialThrottle.Worker/Program.cs
@@ -0,0 +1,7 @@
+using FinancialThrottle.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
