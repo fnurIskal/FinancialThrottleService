870fb260079de54c1736db61b6290056341aadd1
Fatma Nur Iskal <fnuriskal@gmail.com>
2026-06-19 11:42:00 +0300

    fix: remove incorrectly added folder

--- FinancialThrottle.Api/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FinancialThrottle.Api.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
